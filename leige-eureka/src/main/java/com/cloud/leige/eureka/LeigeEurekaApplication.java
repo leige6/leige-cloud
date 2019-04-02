9cc5b4f5956fc1eff01524e643611a02afa26fea
yourleige@gmail.com <Fang201314>
2019-04-02 17:57:18 +0800

    Initial commit

---
+++ leige-eureka/src/main/java/com/cloud/leige/eureka/LeigeEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.cloud.leige.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class LeigeEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeigeEurekaApplication.class, args);
+    }
+
+}
---
+++ leige-gateway/src/test/java/com/cloud/leige/gateway/LeigeGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cloud.leige.gateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LeigeGatewayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
